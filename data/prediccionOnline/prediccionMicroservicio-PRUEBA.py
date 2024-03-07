92b6e200eb7bdfd2ddb9bcf8fb429a2e2d6f7154
castroswork <juliacastroswork@gmail.com>
2024-03-07 12:33:02 +0100

    Archivo ghcr configurado

--- data/prediccionOnline/prediccionMicroservicio-PRUEBA.py
+++ data/prediccionOnline/prediccionMicroservicio-PRUEBA.py
@@ -33,17 +33,17 @@ def predict_eta():
 def predict_delivery(): ## MODIFICADA !! para solo usar time o truckId dado que el modelo solo toma una feature
     
 # Realizar la predicción utilizando solo truckId
-    # data = request.get_json()
-    # truck_id = np.array([data['truckId']])
-    # encoded_truck_id = labelEncoder.transform(truck_id).reshape(-1, 1)
-    # prediccion = modelo_tiempo_entrega.predict(encoded_truck_id)
-    # return jsonify({'prediction': prediccion[0].tolist()})
-
-# Realizar la predicción utilizando solo time
     data = request.get_json()
-    time_features = np.array(data['time']).reshape(1, -1)  # Extraer y dar forma a las características de tiempo
-    prediccion = modelo_tiempo_entrega.predict(time_features) #solo cojo una feature, porque cuando intento tb con truckId me sale error en Postman (arreglar)
-    return jsonify({'prediction': prediccion[0].tolist()})  # Devolver la predicción como respuesta JSON
+    truck_id = np.array([data['truckId']])
+    encoded_truck_id = labelEncoder.transform(truck_id).reshape(-1, 1)
+    prediccion = modelo_tiempo_entrega.predict(encoded_truck_id)
+    return jsonify({'prediction': prediccion[0].tolist()})
+
+# # Realizar la predicción utilizando solo time
+#     data = request.get_json()
+#     time_features = np.array(data['time']).reshape(1, -1)  # Extraer y dar forma a las características de tiempo
+#     prediccion = modelo_tiempo_entrega.predict(time_features) #solo cojo una feature, porque cuando intento tb con truckId me sale error en Postman (arreglar)
+#     return jsonify({'prediction': prediccion[0].tolist()})  # Devolver la predicción como respuesta JSON
 
 if __name__ == '__main__':
     app.run(debug=True, port =7777, host='0.0.0.0')
